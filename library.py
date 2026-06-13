5d359319ca4f65fb6a45e7d863614fa988e4c29a
unknown <itshananworks@gmail.com>
2026-06-13 13:53:00 +0700

    refactor: organize library system into functions

--- library.py
+++ library.py
@@ -2,54 +2,70 @@ import time
 
 buku = []
 
-while True:
-    print("=" * 15, "PERPUSTAKAAN SEDERHANA", "=" * 15)
-    print("1.Lihat Buku")
-    print("2.Tambah Buku")
-    print("3.Hapus Buku")
-    print("4.Keluar Perpustakaan")
-    pilihan_pengguna = input("Masukkan Pilihan Anda: ").lower()
-    if pilihan_pengguna not in [
-        "1", "lihat buku",
-        "2", "tambah buku",
-        "3", "hapus buku",
-        "4", "keluar perpustakaan"
-    ]:
-        print("Pilihan Tidak Valid, Harap Memasukkan Pilihan Yang Valid!!")
-        continue
-    else:
-        if pilihan_pengguna in [
+
+"""TAMPILAN MENU PERPUSTAKAAN"""
+def menu_library():
+    while True:
+        print("=" * 30, "PERPUSTAKAAN SEDERHANA", "=" * 30)
+        print("1.Lihat Buku")
+        print("2.Tambah Buku")
+        print("3.Hapus Buku")
+        print("4.Keluar Perpustakaan")
+        pilihan_pengguna = input("Masukkan Pilihan Anda: ").lower()
+        if pilihan_pengguna not in [
+            "1", "lihat buku",
+            "2", "tambah buku",
+            "3", "hapus buku",
             "4", "keluar perpustakaan"
         ]:
-            print("Terima Kasih Telah Memakai Perpustakaan Kami:D")
-            time.sleep(2)
-            break
-        elif pilihan_pengguna in [
-            "1", "lihat buku"
-        ]:
-            if not buku :
-                print("Belum Ada Buku Yang Ditambahkan")
-            else:
-                for i in range(len(buku)) :
-                    print(i + 1,".", buku[i])
-        elif pilihan_pengguna in [
-            "2", "tambah buku"
-        ]:
-            tambah_buku = input("Masukkan Nama Buku: ")
-            buku.append(tambah_buku)
-            print("YEY! Berhasil Menambahkan Buku:D")
-            time.sleep(2)
-        elif pilihan_pengguna in [
-            "3", "hapus buku"
-        ]:
-            if not buku :
-                print("Tidak Ada Buku Yang Ingin Dihapus")
-            else:
-                hapus_buku = int(input("Masukkan Nomor Buku Yang Anda Ingin Hapus: "))
-                if 1 <= hapus_buku <= len(buku) :
-                    buku.pop(hapus_buku - 1)
-                    print("YEY!! Buku Berhasil Dihapus:D")
-                    time.sleep(1)
-                else:
-                    print("Nomor Buku Tidak Tersedia")
-                    time.sleep(1)
+            print("Pilihan Tidak Valid!!")
+            continue
+        else:
+            if pilihan_pengguna in ["4", "keluar perpustakaan"]:
+                print("Terima Kasih Telah Menggunakan Perpustakaan Kami:D")
+                time.sleep(1)
+                break
+            elif pilihan_pengguna in ["1", "lihat buku"]:
+                lihat_buku()
+            elif pilihan_pengguna in ["2", "tambah buku"]:
+                tambah_buku()
+            elif pilihan_pengguna in ["3", "hapus buku"]:
+                hapus_buku()
+
+
+
+
+
+"""TAMPILAN MENU UNTUK MELIHAT BUKU"""
+def lihat_buku():
+    if not buku:
+        print("Belum Ada Buku Yang Ditambahkan")
+    else:
+        for i in range(len(buku)):
+            print(i + 1,".", buku[i])
+
+"""TAMPILAN MENU UNTUK MENAMBAH BUKU"""
+def tambah_buku():
+    nama_buku = input("Masukkan Nama Buku: ")
+    print("YEY!! Berhasil Menambahkan Buku")
+    time.sleep(1)
+    buku.append(nama_buku)
+
+"""TAMPILAN MENU UNTUK MENGHAPUS BUKU"""
+def hapus_buku():
+    if not buku:
+        print("Tidak Ada Buku Yang Ingin Dihapus!!")
+    else:
+        hapus = int(input("Masukkan Nomor Buku Yang Anda Ingin Hapus: "))
+        if 1 <= hapus <= len(buku):
+            buku.pop(hapus - 1)
+            print("YEY!! Berhasil Menghapus Buku")
+            time.sleep(1)
+        else:
+            print("Nomor Buku Tidak Tersedia")
+            time.sleep(1)
+
+
+
+"""TEMPAT PROSES DATA"""
+menu_library()
